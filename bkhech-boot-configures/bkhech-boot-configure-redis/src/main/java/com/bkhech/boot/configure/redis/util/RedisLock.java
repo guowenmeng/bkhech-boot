b0297fda1cc97a1d73d74092f415829bcae58699
guowm <guowm@5fan.com>
2019-07-04 13:15:34 +0800

    feat: 新增redis分布式锁工具类

---
+++ bkhech-boot-configures/bkhech-boot-configure-redis/src/main/java/com/bkhech/boot/configure/redis/util/RedisLock.java
@@ -0,0 +1,61 @@
+package com.bkhech.boot.configure.redis.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+/**
+ * @author guowm[guowm@5fun.com]
+ * @date 2018/12/26
+ */
+@Component
+public class RedisLock {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RedisLock.class);
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    /**
+     * 加锁
+     * @param key
+     * @param value 当前时间+超时时间
+     * @return
+     */
+    public boolean lock(String key, String value) {
+        if(redisTemplate.opsForValue().setIfAbsent(key, value)) {
+            return true;
+        }
+        String currentValue = redisTemplate.opsForValue().get(key);
+        //如果锁过期
+        if (!StringUtils.isEmpty(currentValue)
+                && Long.parseLong(currentValue) < System.currentTimeMillis()) {
+            //获取上一个锁的时间
+            String oldValue = redisTemplate.opsForValue().getAndSet(key, value);
+            if (!StringUtils.isEmpty(oldValue) && oldValue.equals(currentValue)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 解锁
+     * @param key
+     * @param value
+     */
+    public void unlock(String key, String value) {
+        try {
+            String currentValue = redisTemplate.opsForValue().get(key);
+            if (!StringUtils.isEmpty(currentValue) && currentValue.equals(value)) {
+                redisTemplate.opsForValue().getOperations().delete(key);
+            }
+        }catch (Exception e) {
+            LOGGER.error("【redis分布式锁】解锁异常, {}", e);
+        }
+    }
+
+}

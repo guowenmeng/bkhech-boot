d628ab92e931a382030255da6409be20c7e5fc94
郭文猛 <guowm@5fun.com>
2020-03-09 14:15:17 +0800

    feat: util: 基于redis的分布式锁

--- bkhech-boot-configures/bkhech-boot-configure-redis/src/main/java/com/bkhech/boot/configure/redis/util/RedisLock.java
+++ bkhech-boot-configures/bkhech-boot-configure-redis/src/main/java/com/bkhech/boot/configure/redis/util/RedisLock.java
@@ -7,6 +7,9 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import java.time.Clock;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author guowm[guowm@5fun.com]
  * @date 2018/12/26
@@ -18,6 +21,32 @@ public class RedisLock {
 
     @Autowired
     private StringRedisTemplate redisTemplate;
+
+    /**
+     * 加锁
+     * @param key
+     * @param value 加锁时间(当前时间+超时时间)
+     * @param value timeout 等待时间
+     * @param value unit  等待时间单位
+     * @return
+     */
+    public boolean lock(String key, String value, long timeout, TimeUnit unit) {
+        if(redisTemplate.opsForValue().setIfAbsent(key, value, timeout, unit)) {
+            return true;
+        }
+        String currentValue = redisTemplate.opsForValue().get(key);
+        //如果锁过期
+        if (!StringUtils.isEmpty(currentValue)
+                && Long.parseLong(currentValue) < System.currentTimeMillis()) {
+            //获取上一个锁的时间
+            String oldValue = redisTemplate.opsForValue().getAndSet(key, value);
+            if (!StringUtils.isEmpty(oldValue) && oldValue.equals(currentValue)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     /**
      * 加锁
@@ -56,6 +85,15 @@ public class RedisLock {
         }catch (Exception e) {
             LOGGER.error("【redis分布式锁】解锁异常, {}", e);
         }
+    }
+
+    /**
+     * 锁定时间
+     * @param millis
+     * @return
+     */
+    public String getLockTime(int millis) {
+        return Clock.systemUTC().instant().plusMillis(millis).toEpochMilli() + "";
     }
 
 }

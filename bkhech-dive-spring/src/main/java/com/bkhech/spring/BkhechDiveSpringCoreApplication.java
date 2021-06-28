157b826a883dd397895c707380ce689321c74f37
郭文猛 <guowm@5fun.com>
2021-06-28 11:39:12 +0800

    refactor: add bkhech-dive-spring module

--- bkhech-dive-spring/src/main/java/com/bkhech/spring/BkhechDiveSpringCoreApplication.java
+++ bkhech-dive-spring/src/main/java/com/bkhech/spring/BkhechDiveSpringCoreApplication.java
@@ -1,14 +1,13 @@
-package tools;
+package com.bkhech.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CassandraExampleApplication {
-
+public class BkhechDiveSpringCoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CassandraExampleApplication.class, args);
+        SpringApplication.run(BkhechDiveSpringCoreApplication.class, args);
     }
 
 }
---
+++ bkhech-dive-spring/src/main/java/com/bkhech/spring/core/SpringAspectOrder.java
@@ -0,0 +1,9 @@
+package com.bkhech.spring.core;
+
+/**
+ * filter,interceptor,controllerAdvice,aspect,controller执行顺序
+ * @author guowm
+ * @date 2021/6/2
+ */
+public class SpringAspectOrder {
+}
